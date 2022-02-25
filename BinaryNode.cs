a5da1959adee95b87dfa1930c35f9ae7a4a0319d
Mustafa Özçetin <mustafaozcetin76@yahoo.com>
2022-02-25 12:41:15 +0300

    Initial commit.

---
+++ BinaryNode.cs
@@ -0,0 +1,35 @@
+﻿namespace binary_node1
+{
+    class BinaryNode<T>
+    {
+        public BinaryNode(T value)
+        {
+            Value = value;
+            LeftChild = null;
+            RightChild = null;
+        }
+        public T Value { get; set; }
+
+        public BinaryNode<T> LeftChild { get; set; }
+
+        public BinaryNode<T> RightChild { get; set; }
+
+        public void AddLeft(BinaryNode<T> node)
+        {
+            LeftChild = node;
+        }
+
+        public void AddRight(BinaryNode<T> node)
+        {
+            RightChild = node;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: {1} {2}",
+                Value,
+                LeftChild == null ? "null" : LeftChild.Value,
+                RightChild == null ? "null" : RightChild.Value);
+        }
+    }
+}
