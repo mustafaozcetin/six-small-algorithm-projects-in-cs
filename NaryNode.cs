89244231af7cf8b8f2f4b88f9a0f817685916b31
Mustafa Özçetin <mustafaozcetin76@yahoo.com>
2022-02-27 00:08:53 +0300

    Add files via upload

---
+++ NaryNode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nary_node2
+{
+    class NaryNode<T>
+    {
+        public NaryNode(T value)
+        {
+            Value = value;
+            Children = new List<NaryNode<T>>();
+        }
+        public T Value { get; set; }
+
+        public List<NaryNode<T>> Children { get; set; }
+
+        public void AddChild(NaryNode<T> node)
+        {
+            Children.Add(node);
+        }
+
+        public override string ToString()
+        {
+            return ToString("  ");
+        }
+
+        private string ToString(string spaces)
+        {
+            string newLine = Environment.NewLine;
+            string stringValue = $"{spaces}{Value}:{newLine}";
+
+            foreach (NaryNode<T> childNode in Children)
+            {
+                stringValue += childNode.ToString(spaces + "  ");
+            }
+            return stringValue;
+        }
+    }
+}
