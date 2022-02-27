56d79702f433e9f55395ac06ab70c74b5b207169
Mustafa Özçetin <mustafaozcetin76@yahoo.com>
2022-02-27 20:10:01 +0300

    Initial commit.

---
+++ trees/BinaryNode.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace binary_node3
+{
+    class BinaryNode<T>
+    {
+        public BinaryNode(T value)
+        {
+            Value = value;
+            LeftChild = null;
+            RightChild = null;
+        }
+        public T Value { get; set; }
+
+        public BinaryNode<T> LeftChild { get; set; }
+
+        public BinaryNode<T> RightChild { get; set; }
+
+        public void AddLeft(BinaryNode<T> node)
+        {
+            LeftChild = node;
+        }
+
+        public void AddRight(BinaryNode<T> node)
+        {
+            RightChild = node;
+        }
+
+        public BinaryNode<T> FindNode(T valueToFind)
+        {
+            if (Value.Equals(valueToFind))
+            {
+                return this;
+            }
+            if (LeftChild != null)
+            {
+                var node = LeftChild.FindNode(valueToFind);
+                if (node != null)
+                {
+                    return node;
+                }
+            }
+            if (RightChild != null)
+            {
+                var node = RightChild.FindNode(valueToFind);
+                if (node != null)
+                {
+                    return node;
+                }
+            }
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return ToString("  ");
+        }
+
+        private string ToString(string spaces)
+        {
+            string newLine = Environment.NewLine;
+            string stringValue = $"{spaces}{Value}: {newLine}";
+            const string nullNodeValue = "None";
+
+            if (LeftChild == null && RightChild == null)
+            {
+                return stringValue;
+            }
+
+            if (LeftChild != null)
+            {
+                stringValue += LeftChild.ToString(spaces + "  ");
+            }
+            else
+            {
+                stringValue += $"{spaces}  {nullNodeValue}{newLine}";
+            }
+
+            if (RightChild != null)
+            {
+                stringValue += RightChild.ToString(spaces + "  ");
+            }
+            else
+            {
+                stringValue += $"{spaces}  {nullNodeValue}{newLine}";
+            }
+
+            return stringValue;
+        }
+    }
+}
---
+++ trees/NaryNode.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nary_node3
+{
+    class NaryNode<T>
+    {
+        public NaryNode(T value)
+        {
+            Value = value;
+            Children = new List<NaryNode<T>>();
+        }
+        public T Value { get; set; }
+
+        public List<NaryNode<T>> Children { get; set; }
+
+        public void AddChild(NaryNode<T> node)
+        {
+            Children.Add(node);
+        }
+
+        public NaryNode<T> FindNode(T valueToFind)
+        {
+            if (Value.Equals(valueToFind))
+            {
+                return this;
+            }
+            foreach (var childNode in Children)
+            {
+                var node = childNode.FindNode(valueToFind);
+                if (node != null)
+                {
+                    return node;
+                }
+            }
+
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return ToString("  ");
+        }
+
+        private string ToString(string spaces)
+        {
+            string newLine = Environment.NewLine;
+            string stringValue = $"{spaces}{Value}:{newLine}";
+
+            foreach (NaryNode<T> childNode in Children)
+            {
+                stringValue += childNode.ToString(spaces + "  ");
+            }
+            return stringValue;
+        }
+    }
+}
