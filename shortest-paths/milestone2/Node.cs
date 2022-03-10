fa24130f133fdf393464072d24edf28f7caf522a
Mustafa Özçetin <mustafaozcetin76@yahoo.com>
2022-03-10 18:51:57 +0300

    Initial commit.

---
+++ shortest-paths/milestone2/Node.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace draw_network
+{
+    public class Node
+    {
+        public Node(Network network, Point center, string text)
+        {
+            Index = -1;
+            Network = network;
+            Center = center;
+            Text = text;
+            Links = new List<Link>();
+
+            Network.AddNode(this);
+        }
+
+        public int Index { get; set; }
+        
+        public Network Network { get; }
+        
+        public Point Center { get; }
+        
+        public string Text { get; }
+        
+        public List<Link> Links { get; }
+
+        public void AddLink(Link link)
+        {
+            Links.Add(link);
+        }
+
+        public void Draw(Canvas canvas)
+        {
+            Rect nodeBounds = new Rect(Center.X - 10, Center.Y - 10, 20, 20);
+            canvas.DrawEllipse(nodeBounds, Brushes.White, Brushes.Black, 1);
+
+            canvas.DrawString(Text, 20, 20, Center, 0, 12, Brushes.Blue);
+        }
+
+        public override string ToString()
+        {
+            return $"[{Text}]";
+        }
+    }
+}
---
+++ shortest-paths/milestone2/Window1.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace draw_network
+{
+    public partial class Window1
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private Network MyNetwork = new Network();
+
+        private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.DefaultExt = ".net";
+                dialog.Filter = "Network Files|*.net|All Files|*.*";
+
+                // Display the dialog.
+                bool? result = dialog.ShowDialog();
+                if (result == true)
+                {
+                    // Open the network.
+                    MyNetwork = new Network(dialog.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MyNetwork = new Network();
+            }
+
+            // Display the network.
+            DrawNetwork();
+        }
+
+        private void DrawNetwork()
+        {
+            // Remove any previous drawing.
+            mainCanvas.Children.Clear();
+
+            // Make the network draw itself.
+            MyNetwork.Draw(mainCanvas);
+
+            MyNetwork.Links.ForEach(link => link.Draw(mainCanvas));
+            MyNetwork.Links.ForEach(link => link.DrawLabel(mainCanvas));
+            MyNetwork.Nodes.ForEach(node => node.Draw(mainCanvas));
+        }
+
+        private void ExitCommand_Executed(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
